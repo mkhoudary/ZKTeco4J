54c24e6215846e9ffb616666e9878b6abd71696f
mkhoudary <m.elkhoudary@gmail.com>
2021-02-22 22:57:02 +0200

    Prepared method to read response

--- src/main/java/ps/purelogic/zkteco4j/terminal/ZKTerminal.java
+++ src/main/java/ps/purelogic/zkteco4j/terminal/ZKTerminal.java
@@ -11,6 +11,7 @@ import java.io.OutputStream;
 import java.net.Socket;
 import ps.purelogic.zkteco4j.commands.CommandCode;
 import ps.purelogic.zkteco4j.commands.ZKCommand;
+import ps.purelogic.zkteco4j.utils.HexUtils;
 
 /**
  *
@@ -51,6 +52,10 @@ public class ZKTerminal {
         os.flush();
 
         replyNo++;
+        
+        int[] response = readResponse();
+        
+        System.out.println("Resp " + HexUtils.bytesToHex(response));
     }
 
     public void disconnect() throws IOException {
@@ -64,9 +69,42 @@ public class ZKTerminal {
 
         replyNo++;
         
+        int[] response = readResponse();
+        
+        System.out.println("Resp " + HexUtils.bytesToHex(response));
+
         is.close();
         os.close();
         socket.close();
+    }
+
+    private int[] readResponse() throws IOException {
+        int index = 0;
+        int[] data = new int[1000000];
+
+        int read;
+        int size = 0;
+
+        boolean reading = true;
+
+        while (reading && (read = is.read()) != -1) {
+            if (index >= 4 && index <= 7) {
+                size += read * Math.pow(16, index - 4);
+            } else if (index > 7) {
+                if (index - 7 > size) {
+                    reading = false;
+                }
+            }
+
+            data[index] = read;
+            index++;
+        }
+
+        int[] finalData = new int[index];
+
+        System.arraycopy(data, 0, finalData, 0, index);
+
+        return finalData;
     }
 
 }
